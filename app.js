560484bbf27e36a6a2ee22c915c682c0ce702a41
pooh <cktjdgus45@naver.com>
2022-01-25 14:42:41 +0900

    add store todos and render todos

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const todoInput = document.querySelector('.todo-input');
 
 //user login
 
-checkUserState(); //for when browser loaded render
+if (checkUserState()) {//for when browser loaded render
+    renderPaintTodo();  //render todos in localStrage
+}
 
 function handleLogin(event) {
     event.preventDefault();
@@ -30,11 +32,50 @@ function checkUserState() {
 
 function loginState() {
     loginForm.classList.add('hide');
+    todoForm.classList.replace('hide', 'show');
 }
 function logoutState() {
-    todoForm.classList.add('show');
+    loginForm.classList.add('show');
+    todoForm.classList.replace('show', 'hide');
 }
 
+//submit todo
+function renderRealTime(todo) {
+    const ul = document.querySelector('.todos');
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = todo;
+    li.appendChild(span);
+    ul.appendChild(li);
+}
 
+function renderPaintTodo() { //for when browser loaded render
+    const todos = JSON.parse(localStorage.getItem('todo')) || [];
+    const ul = document.querySelector('.todos');
+    todos.map(item => {
+        const { todo } = item;
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.innerText = todo;
+        li.appendChild(span);
+        ul.appendChild(li);
+    })
+}
+
+function handleTodoSubmit(event) {
+    const todos = JSON.parse(localStorage.getItem('todo')) || [];
+    event.preventDefault();
+    const todo = {
+        id: todos.length + 1,
+        todo: todoInput.value
+    };
+    const updated = [...todos, todo];
+    console.log(updated);
+    localStorage.setItem('todo', JSON.stringify(updated));
+    renderRealTime(todoInput.value); //for realtime call function
+    todoInput.value = "";
+}
+
+todoForm.addEventListener('submit', handleTodoSubmit);
 
 
